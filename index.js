75179ccb2bb4516d81fcc6350fa8adedc1ac7b0f
rngo1214 <rngo1214@gmail.com>
2018-10-07 00:04:39 -0700

    Figured out ajax

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const schedule = require('node-schedule');
 const moment = require('moment');
 const momenttz = require('moment-timezone');
+const path = require("path");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -24,6 +25,10 @@ mongoose
 //load models
 require('./models/index.js');
 const routes = require("./api/index");
+app.use(express.static(path.join(__dirname, "/html")));
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, "/html/Welcome.html"));
+})
 app.use("/api/event", routes.events);
 app.listen(5000, ()=>{
   console.log("Hello");
